ad1dbb872d4d94f06ad7eabf77b6604181d2ffe5
Matt Cadorette <mcadorette@gmail.com>
2018-11-30 16:10:36 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/nv01/Nv01Application.java
@@ -0,0 +1,12 @@
+package com.nv01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nv01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nv01Application.class, args);
+	}
+}
---
+++ src/test/java/com/nv01/Nv01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nv01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Nv01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
